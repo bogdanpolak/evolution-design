bdf8f107280c139bc7ac71dada2b1a5ff67b6eee
Bogdan Polak <bogdanpol@gmail.com>
2020-04-21 23:03:03 +0100

    Improved pizza demo

--- pizza/FactoriesEvolution.cs
+++ pizza/FactoriesEvolution.cs
@@ -6,24 +6,24 @@ public interface IPizza {}
 public class Foo
 {
     public IPizza _pizza;
+    Func<string,IPizza> simpleFactoryDelegate = (pizzaName) => new SimplePizza(pizzaName);
     
     public void BuildPizza_SimpleFactory()
     {
-        Func<string,IPizza> simpleFactoryDelegate = (pizzaName => new SimplePizza(pizzaName));
-        _pizza = simpleFactoryDelegate("menu-item-3");
+        _pizza = simpleFactoryDelegate("Hawaiian");
     }
     
     public void BuildPizza_FactoryMethod()
     {
-        _pizza = _pizzaFactory.build("menu-item-3");
+        _pizza = _pizzaFactory.build("Hawaiian");
     }
     
     public void BuildPizza_AbstractFactory()
     {
         _pizza = new Pizza(
-            _pizzaAbstractFactory.buildCrust("chicago-style"),
+            _pizzaAbstractFactory.buildCrusts("chicago-style"),
             _pizzaAbstractFactory.buildCheese("mozzarella"),
-            _pizzaAbstractFactory.buildTopping(new[]{"mozzarella","ham"})
+            _pizzaAbstractFactory.buildTopping(new[]{"pinapple","ham"})
         );
     }
     
